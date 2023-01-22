f5e263e298e6b1d9424a1767252558008b403900
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2023-01-22 04:27:06 -0600

    feat: initial commit
    This commit includes setting up a basic VyOS router.

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    hosts = (pkgs.formats.yaml {}).generate "hosts.yaml" {
+      routers = {
+        hosts = {
+          router_main = {
+            ansible_host = "192.168.2.1";
+            ansible_user = "vyos";
+            ansible_network_os = "vyos";
+            ansible_connection = "network_cli";
+          };
+        };
+      };
+    };
+  in {
+    devShell.${system} = pkgs.mkShell {
+      ANSIBLE_INVENTORY = "${hosts}";
+      packages = [
+        pkgs.ansible
+      ];
+    };
+  };
+}

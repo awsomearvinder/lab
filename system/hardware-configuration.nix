9e0c828ea38bf8af3f27fc5624f0a613793f68d6
Arvinder <ArvinderDhan@gmail.com>
2023-04-21 19:27:22 -0500

    feat: allow LISH to work.
    This configures the console serial device LISH uses.

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -3,6 +3,12 @@
   boot.loader.grub.device = "nodev";
   boot.loader.timeout = 10;
   boot.loader.grub.forceInstall = true;
+  boot.kernelParams = ["console=ttyS0,19200n8"];
+  boot.loader.grub.extraConfig = ''
+    serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+    terminal_input serial;
+    terminal_output serial
+  '';
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi"];
   boot.initrd.kernelModules = ["nvme"];
   fileSystems."/" = {

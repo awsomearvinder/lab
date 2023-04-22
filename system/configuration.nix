567ddbc1c8dc63f6ff61d91d089061ad70ae5752
Arvinder <ArvinderDhan@gmail.com>
2023-04-21 19:25:50 -0500

    fix outgoing IPv6 traffic: disable tempAddresses
    Linode blocks all outgoing traffic that isn't from your assigned SLAAC
    ip address, privacy extensions don't work.

--- system/configuration.nix
+++ system/configuration.nix
@@ -8,6 +8,7 @@
   boot.cleanTmpDir = true;
   zramSwap.enable = false;
   networking.hostName = "linode-nixos";
+  networking.tempAddresses = "disabled";
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbJuiw1aOPykLtDP2hQCEifKP2QBwh+wJ0ktYjv6S7P bender@desktop"

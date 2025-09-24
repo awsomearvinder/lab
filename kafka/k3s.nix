f26613266e99861d7ca6f3a2f993c99ad006953d
Arvinder Dhanoa <git@mail.arvinderd.com>
2025-09-24 02:02:48 -0500

    feat(kafka/k3s): init.

---
+++ kafka/k3s.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  networking.firewall.allowedTCPPorts = [
+    6443
+  ];
+  services.k3s.enable = true;
+  services.k3s.role = "server";
+  services.k3s.extraFlags = toString [
+    "--debug"
+    "--tls-san kafka.arvinderd.com"
+  ];
+}

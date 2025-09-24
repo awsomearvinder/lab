6e48559edac912d408482af2f61304b7b051a9fa
Arvinder Dhanoa <git@mail.arvinderd.com>
2025-09-24 02:02:48 -0500

    feat(lib/base.nix): support authorized key auth for root.
    Most my servers are probably going to stop having a dedicated user account
    soon...

--- lib/base.nix
+++ lib/base.nix
@@ -4,5 +4,8 @@
     ./ddns.nix
   ];
 
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILNQHSuX71ZhzbtkBZV4RCHBdfZ+JUIR1hKfB9gRzlDE"
+  ];
   networking.domain = lib.mkDefault "arvinderd.com";
 }

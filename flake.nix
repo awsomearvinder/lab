48b1c08536600e8ca2736cd4968b92771be7a074
Arvinder Dhanoa <git@mail.arvinderd.com>
2026-04-05 13:18:37 -0500

    feat(flake.nix): remove talm.
    We don't need or use this stuff.

--- flake.nix
+++ flake.nix
@@ -21,22 +21,11 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      talm = pkgs.buildGoModule {
-        name = "talm";
-        vendorHash = "sha256-ykzJ2qt/ps+zpMd2g939CDV6qAfLbUD5ZhU0RAM/9Zw=";
-        src = pkgs.fetchFromGitHub {
-          owner = "cozystack";
-          repo = "talm";
-          rev = "v0.15.0";
-          hash = "sha256-egTHXAUdzE7jRXQZ4JFYiOht6lWEiqY7ClQ9XdFJB8c=";
-        };
-      };
     in
     {
 
       devShells.${system}.default = pkgs.mkShell {
         packages = [
-          talm
         ];
       };
 

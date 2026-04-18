e7a8b09a3f6eb615c6359be32be6e268874083a8
Arvinder Dhanoa <git@mail.arvinderd.com>
2026-04-17 21:22:57 -0500

    feat(herta/): reverse proxy to LXC for the public internets
    I hates teh IPv4 world

--- herta/applications/caddy.nix
+++ herta/applications/caddy.nix
@@ -1,8 +1,27 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  proxy_mapping = {
+    "test" = "test.seele";
+  };
+in
 {
   services.caddy = {
     enable = true;
     package = pkgs.caddy;
+    virtualHosts = lib.attrsets.concatMapAttrs (new_name: real_domain: {
+      "${new_name}.${config.networking.hostName}.${config.networking.domain}" = {
+        extraConfig = ''
+          reverse_proxy https://${real_domain}.${config.networking.domain} {
+            header_up Host Host ${real_domain}.${config.networking.domain}
+          }
+        '';
+      };
+    }) proxy_mapping;
   };
   networking.firewall.allowedTCPPorts = [
     443

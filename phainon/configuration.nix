23f77bca41ab4d81f38eb005c7a94aa33c30f1c4
Arvinder Dhanoa <git@mail.arvinderd.com>
2026-05-29 19:33:39 -0500

    chore(phainon): OP John_Benber

--- phainon/configuration.nix
+++ phainon/configuration.nix
@@ -90,6 +90,7 @@
       USE_SIMD_FLAGS = "TRUE";
       ENABLE_WHITELIST = "TRUE";
       WHITELIST = "John_Benber,xXScam42069Xx,IMM3RSIVE,9rtyt,Geigus,ssommerai,sekahauwu,KattmannPlayzz";
+      OPS = "John_Benber";
       CF_SLUG = "integrated-minecraft";
       CF_FILE_ID = "7926313";
       CF_API_KEY_FILE = "/cf.key";
